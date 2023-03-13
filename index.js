047db267b7468efdf6fe78c2f3c64d8ea51e828a
milhan omar <milhanomarosman@gmail.com>
2023-03-13 10:05:15 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+// const writeCards=(["Guadalupe", "Ollie", "Aki"] , "surprise")
+function writeCards(name,surprise){
+ const thisArray =[];
+ for(let i = 0; i < name.length; i++ ){
+ const message = `Thank you, ${name[i]}, for the wonderful ${surprise} gift`
+ thisArray.push(message);
+ }
+ return thisArray;
+}
+function countDown(nums){
+ do{
+  console.log(nums);
+  nums--;
+ } 
+ while(nums >= 0);
+}
