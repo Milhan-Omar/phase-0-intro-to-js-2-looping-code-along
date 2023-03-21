cdcaf25f5269e046e50eece735b53eefc1cc896e
Milhan Osman <milhanomarosman@gmail.com>
2023-03-21 15:45:37 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 // Code your solutions in this file
 // const writeCards=(["Guadalupe", "Ollie", "Aki"] , "surprise")
-function writeCards(name,surprise){
- const thisArray =[];
- for(let i = 0; i < name.length; i++ ){
- const message = `Thank you, ${name[i]}, for the wonderful ${surprise} gift`
- thisArray.push(message);
+// function writeCards(name,surprise){
+//  const thisArray =[];
+//  for(let i = 0; i < name.length; i++ ){
+//  const message = `Thank you, ${name[i]}, for the wonderful ${surprise} gift`
+//  thisArray.push(message);
+//  }
+//  return thisArray;
+// }
+// function countDown(nums){
+//  do{
+//   console.log(nums);
+//   nums--;
+//  } 
+//  while(nums >= 0);
+// }
+function writeCards(name, event) {
+ const myArray = [];
+for (let i = 0; i<name.length; i++) {
+const message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+ myArray.push(message) ;
  }
- return thisArray;
+ return   myArray;
 }
-function countDown(nums){
- do{
-  console.log(nums);
-  nums--;
- } 
- while(nums >= 0);
+function countDown(num) {
+  do {
+  console.log(num);
+  num--;
+ }
+ while (num >= 0)
 }
